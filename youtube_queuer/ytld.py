bb3aacfd7a4946a18ee065c0fd9b083780d52184
Simon Walker <s.r.walker101@googlemail.com>
2018-05-14 21:08:08 +0100

    Add basic handler

--- youtube_queuer/ytld.py
+++ youtube_queuer/ytld.py
@@ -1,6 +1,13 @@
 from aiohttp import web
 
 
+async def worker_next(request):
+    return web.Response(text='Hello world')
+
+
 def main(args):
     app = web.Application()
+    app.add_routes([
+        web.get('/worker/next', worker_next),
+        ])
     web.run_app(app, host=args.host, port=args.port)
