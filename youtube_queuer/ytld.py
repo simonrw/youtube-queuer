3b60cb6cefacb2d9b21330cf19516d09b17e1c4e
Simon Walker <s.r.walker101@googlemail.com>
2018-05-14 21:10:32 +0100

    Use routes decorator

--- youtube_queuer/ytld.py
+++ youtube_queuer/ytld.py
@@ -1,13 +1,15 @@
 from aiohttp import web
 
 
+routes = web.RouteTableDef()
+
+
+@routes.get('/worker/next')
 async def worker_next(request):
     return web.Response(text='Hello world')
 
 
 def main(args):
     app = web.Application()
-    app.add_routes([
-        web.get('/worker/next', worker_next),
-        ])
+    app.add_routes(routes)
     web.run_app(app, host=args.host, port=args.port)
