8caaf5259a814017d26de95699f8ae2a0d18dd82
Simon Walker <s.r.walker101@googlemail.com>
2018-05-14 21:41:19 +0100

    Fetch latest result

--- youtube_queuer/ytld.py
+++ youtube_queuer/ytld.py
@@ -7,7 +7,24 @@ app = Flask('ytld')
 
 @app.route('/worker/next')
 def next():
-    return jsonify({})
+    with sqlite3.connect('db.db') as c:
+        cur = c.cursor()
+        cur.execute('''select arguments from yt_queue
+        order by added desc
+        limit 1''')
+        rows = cur.fetchone()
+        if rows:
+            args = {
+                    'status': 'ok',
+                    'status_code': 0,
+                    'arguments': rows[0],
+                    }
+        else:
+            args = {
+                    'status': 'no-results',
+                    'status_code': 1,
+                    }
+        return jsonify(args)
 
 
 def main(args):
