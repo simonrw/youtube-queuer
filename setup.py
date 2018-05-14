11a4e9cfaf68f6e45bcf1bb86c2eb85f2818929f
Simon Walker <s.r.walker101@googlemail.com>
2018-05-14 20:46:25 +0100

    Start python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='youtube-queuer',
+        description='Download lots of youtube videos',
+        version='0.0.1',
+        author='Simon Walker',
+        author_email='s.r.walker101@googlemail.com',
+        packages=find_packages(),
+        entry_points={
+            'console_scripts': [
+                'ytld=youtube_queuer:ytld_main',
+                ],
+            },
+        )
---
+++ youtube_queuer/__init__.py
@@ -0,0 +1,2 @@
+def ytld_main():
+    pass
