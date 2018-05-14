e2d5c1e9e5e84e5227227eec0d0d0f769361f512
Simon Walker <s.r.walker101@googlemail.com>
2018-05-14 20:30:36 +0100

    Start basic server

--- src/bin/ytld.rs
+++ src/bin/ytld.rs
@@ -1,7 +1,12 @@
 #[macro_use]
 extern crate structopt;
+extern crate futures;
+extern crate hyper;
 extern crate youtube_queuer as yt;
 
+use futures::future::Future;
+use hyper::header::ContentLength;
+use hyper::server::{Http, Request, Response, Service};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -12,8 +17,33 @@ struct Opt {
     host: Option<String>,
 }
 
+struct Ytld;
+
+const PHRASE: &'static str = "Hello world";
+
+impl Service for Ytld {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, _req: Request) -> Self::Future {
+        println!("Got request");
+        Box::new(futures::future::ok(
+            Response::new()
+                .with_header(ContentLength(PHRASE.len() as _))
+                .with_body(PHRASE),
+        ))
+    }
+}
+
 fn main() {
     let opt: Opt = Opt::from_args();
     let port = opt.port.unwrap_or(yt::DEFAULT_PORT);
     let host = opt.host.unwrap_or(yt::DEFAULT_HOST.to_string());
+
+    let addr = format!("{}:{}", host, port).parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(Ytld)).unwrap();
+    server.run().unwrap();
 }
